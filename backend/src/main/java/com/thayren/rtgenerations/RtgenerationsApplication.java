5d7a68c928bc43b9f42174024d90739acca3e002
Renan Duarte <renandg.axl@gmail.com>
2022-12-21 14:07:49 -0300

    Spring Boot project creation and initial settings

---
+++ backend/src/main/java/com/thayren/rtgenerations/RtgenerationsApplication.java
@@ -0,0 +1,13 @@
+package com.thayren.rtgenerations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtgenerationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtgenerationsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thayren/rtgenerations/RtgenerationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thayren.rtgenerations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtgenerationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
